c2420f69724e40f6dcdf162243611aa6d5f610b6
yogithi <j.thielemann@web.de>
2023-11-15 23:57:37 +0100

    erster Sprint als jpynb
    erster Sprint: Nutzung der Tankerkoenig API, Ermittlung der GPS Koordinaten aus einer Adresse und einer ersten GUI

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Sprit
+15.11.2023 JT
+
+ersten Prototyp erstellt als "TK_Sprint1.jpynb" und "TK_Sprint1.py".
+Vor der Nutzung sind Bibliotheken zu installieren:
+pip install pysimplegui
+pip install pytankerkoenig
+pip install geopy
+
+Die angezeigten Daten sind noch als Rohdaten im Json Format. Der nächste Schritt wäre die Daten zu in eine anzeigbare Form zu bringen.
+
+Links zu Dokumentationen:
+
+PySimpleGUI
+https://github.com/PySimpleGUI/PySimpleGUI/tree/master/DemoPrograms
+
+Homepage & Docs
+https://www.pysimplegui.org/en/latest/
+
+Tankerkoenig
+pytankerkoenig auf github
+https://github.com/ultrara1n/pytankerkoenig
+
+Beschreibung pytankerkoenig
+https://pypi.org/project/pytankerkoenig/
+
+API Beschreibung von Tankerkoenig direkt
+https://creativecommons.tankerkoenig.de/?page=info
+
+GeoPy
+https://geopy.readthedocs.io/en/stable/
+
+--------------------------------------------------------------------------------
---
+++ TK_Sprint1.py
@@ -0,0 +1,53 @@
+import PySimpleGUI as sg
+import pytankerkoenig as tk
+from geopy.geocoders import Nominatim
+
+def adresse_zu_gps(adresse):
+    geolocator = Nominatim(user_agent="FH_SWF")
+    location = geolocator.geocode(adresse)
+    if location:
+        return (location.latitude, location.longitude)
+    else:
+        return None
+    
+layout = [
+    [sg.Text('Entfernung'), 
+     sg.Spin(['2km','5km','10km'], key = '-Distance-')
+    ],
+    [sg.Text()],
+    [sg.Text('Adresse:'),
+     sg.Input(key = '-Input-')
+    ],
+    [sg.Text('')],
+    [sg.Text('Ergebnis')],
+    [sg.Multiline("", size = (150,40),disabled=True, key = "-ML-")],
+    [sg.Button('Suchen', key = '-Button1-')]
+]
+
+window = sg.Window('Tankstellen suchen', layout)
+
+while True:
+    event, values = window.read()
+    
+    if event == sg.WIN_CLOSED:
+        break;
+    
+    if event == '-Button1-':
+        adr = values['-Input-']
+        dist = values['-Distance-']
+
+        match values['-Distance-']:
+            case '2km':
+                dist = float(2)
+            case '5km':
+                dist = 5
+            case '10km':
+                dist = 10
+
+        lat, long = adresse_zu_gps(adr)
+
+        data = tk.getNearbyStations('532bec4c-380c-6f32-d8c7-6a11d0d8b2d5',float(lat),float(long),dist,'all','dist')
+
+        window['-ML-'].update(data)
+
+window.close
