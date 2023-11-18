21bdc4458772db1d5d8455e2176a5859b4b7d82a
Benjamin Bruch <benjamin.bruch@gmail.com>
2023-11-18 16:51:48 +0100

    [FIXED] wrong argument names

--- sprit/model/station_model.py
+++ sprit/model/station_model.py
@@ -1,5 +1,5 @@
 class Station:
-    def __init__(self, id, name, brand, street, place, lat, lng, dist, diesel, e5, e10, is_open, house_number, post_code):
+    def __init__(self, id, name, brand, street, place, lat, lng, dist, diesel, e5, e10, isOpen, houseNumber, postCode):
         self.id = id
         self.name = name
         self.brand = brand
@@ -11,9 +11,9 @@ class Station:
         self.diesel = diesel
         self.e5 = e5
         self.e10 = e10
-        self.isOpen = is_open
-        self.houseNumber = house_number
-        self.postCode = post_code
+        self.is_open = isOpen
+        self.house_number = houseNumber
+        self.post_code = postCode
 
     def __str__(self):
-        return f"ID: {self.id}\nName: {self.name}\nBrand: {self.brand}\nStreet: {self.street}\nPlace: {self.place}\nLatitude: {self.lat}\nLongitude: {self.lng}\nDistance: {self.dist} km\nDiesel Price: {self.diesel} €\nE5 Price: {self.e5} €\nE10 Price: {self.e10} €\nIs Open: {self.isOpen}\nHouse Number: {self.houseNumber}\nPost Code: {self.postCode}"
+        return f"ID: {self.id}\nName: {self.name}\nBrand: {self.brand}\nStreet: {self.street}\nPlace: {self.place}\nLatitude: {self.lat}\nLongitude: {self.lng}\nDistance: {self.dist} km\nDiesel Price: {self.diesel} €\nE5 Price: {self.e5} €\nE10 Price: {self.e10} €\nIs Open: {self.is_open}\nHouse Number: {self.house_number}\nPost Code: {self.post_code}"
