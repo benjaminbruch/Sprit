a24f7320d24b7d67c4df2e1192204fb093cd8f55
Benjamin Bruch <benjamin.bruch@gmail.com>
2024-01-08 23:22:16 +0100

    [UPDATED] to fit style

--- sprit/view/station_data_analytics_view.py
+++ sprit/view/station_data_analytics_view.py
@@ -12,6 +12,7 @@ class StationDataAnalyticsView(customtkinter.CTkFrame):
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
         #Mock data
         self.prices = np.random.uniform(1.65, 1.70, 14)
@@ -22,11 +23,24 @@ class StationDataAnalyticsView(customtkinter.CTkFrame):
         self.chart_frame.grid(row=0, column=0, sticky='nsew')
         self.create_chart(self.dates, self.prices, self.chart_frame)
 
+
+        self.average_recommendation_frame = customtkinter.CTkFrame(self)
+        self.average_recommendation_frame.grid(row=0, column=1, sticky='nsew')
+
+        self.average_price_label = customtkinter.CTkLabel(self.average_recommendation_frame, text="Durchschnittspreis: ", font=("Arial", 12))
+        self.average_price_label.grid(row=0, column=1, sticky='nsew')
+
+        self.recommendation_label = customtkinter.CTkLabel(self.average_recommendation_frame, text="Empfehlung: ", font=("Arial", 12))
+        self.recommendation_label.grid(row=1, column=1, sticky='nsew')
+
+
     def create_chart(self, dates, prices, frame):
         min_price = min(prices) - 0.01
         max_price = max(prices) + 0.01
 
         fig, ax = plt.subplots()
+        # Adjust the subplot parameters
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
         fig.set_facecolor('#323333')
         ax.set_facecolor('#323333')
@@ -43,7 +57,10 @@ class StationDataAnalyticsView(customtkinter.CTkFrame):
 
         # Set the labels and title
         ax.tick_params(axis='x', colors='white')
-        plt.xticks(rotation=45)
+
+        every_second_date = dates[::2]  # Slice the list to get every second date
+
+        plt.xticks(every_second_date)
 
         # Remove the frame
         ax.spines['top'].set_visible(False)
@@ -57,4 +74,4 @@ class StationDataAnalyticsView(customtkinter.CTkFrame):
         canvas_widget.grid(row=0, column=0, sticky='nsew')
         canvas_widget.grid_columnconfigure(index=0, weight=1)
         canvas_widget.grid_propagate(False)  # Prevent the widget from changing its size due to its children
-        canvas_widget.configure(height=250)
+        canvas_widget.configure(height=150)
