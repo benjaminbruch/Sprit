5c59ce9eb5497a03cfadf49c6a60e5cb8cdba09d
Benjamin Bruch <benjamin.bruch@gmail.com>
2024-01-21 06:34:57 +0100

    [REFACTORED] db import
    added csv data

--- sprit/data/imp_hist_tk_Data.py
+++ sprit/data/imp_hist_tk_Data.py
@@ -1,17 +1,18 @@
 import csv
+import os
 import sqlite3
+from sprit.resources.credentials import Credentials
 
-# Pfad zur CSV-Datei und SQLite-Datenbank
-prices_file = '/Users/joerg/Programming/AKI/KI Programmierung/Sprit/sprit/data/2023-12-01-prices.csv'
-stations_file = '/Users/joerg/Programming/AKI/KI Programmierung/Sprit/sprit/data/2023-12-01-stations.csv'
-sqlite_db = '/Users/joerg/Programming/AKI/KI Programmierung/Sprit/sprit/data/tk_hist.db'
+# Path to the CSV file and SQLite database
+prices_path = Credentials.csv_prices_path
+sqlite_db = Credentials.db_path
 
 
-# Funktion zum Erstellen einer SQLite-Datenbank und Tabelle
+# Function to create an SQLite database and table
 def create_tables():
     conn = sqlite3.connect(sqlite_db)
     cursor = conn.cursor()
-    
+
     cursor.execute('''CREATE TABLE IF NOT EXISTS prices (
                         date TEXT NOT NULL,
                         time TEXT NOT NULL,
@@ -23,70 +24,44 @@ def create_tables():
                         e5change INTEGER,
                         e10change INTEGER
                     )''')
-    
+
     conn.commit()
-    
-    cursor.execute('''CREATE TABLE IF NOT EXISTS stations (
-                        uuid TEXT NOT NULL UNIQUE,
-                        name TEXT,
-                        brand TEXT,
-                        street TEXT,
-                        house_number TEXT,
-                        post_code TEXT,
-                        city TEXT,
-                        lat REAL,
-                        long REAL,
-                        first_active TEXT,
-                        openingtimes TEXT
-                    )''')
-    
-    cursor.execute('''CREATE INDEX IF NOT EXISTS uuid_ind ON stations (uuid ASC)''')
-                   
-    conn.commit()
-    
     conn.close()
 
 
-# Funktion zum Einfügen von Daten aus der CSV-Datei in die SQLite-Datenbank
+# Function to insert data from the CSV file into the SQLite database
 def insert_data_from_csv():
     conn = sqlite3.connect(sqlite_db)
     cursor = conn.cursor()
 
-    with open(prices_file, 'r', newline='', encoding='utf-8') as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)  # Überspringe den Header
-        for row in csv_reader:
-            inp = row
-            # Datum und Zeit aus csv-input separieren 
-            dat = row[0][:10]
-            tim = row[0][11:19]
-            # Datum und Zeit in getrennte Felder der Datenbank schreiben
-            del row[0]
-            row.insert(0, tim)
-            row.insert(0, dat)
+    for prices_file in os.listdir(prices_path):
+        with open(prices_file, 'r', newline='', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)  # Skip the header
+            for row in csv_reader:
+                inp = row
+                # Separate date and time from csv input
+                dat = row[0][:10]
+                tim = row[0][11:19]
+                # Write date and time into separate fields in the database
+                del row[0]
+                row.insert(0, tim)
+                row.insert(0, dat)
 
-            cursor.execute('INSERT INTO prices (date, time, station_uuid, diesel, e5, e10, dieselchange, e5change, e10change) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
-
-    conn.commit()
-
-    with open(stations_file, 'r', newline='', encoding='utf-8') as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)  # Überspringe den Header
-        for row in csv_reader:
-            try:
-                cursor.execute('INSERT INTO stations (uuid, name, brand, street, house_number, post_code, city, lat, long, first_active, openingtimes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', row)
-            except:
-                continue
+                cursor.execute(
+                    'INSERT INTO prices (date, time, station_uuid, diesel, e5, e10, dieselchange, e5change, e10change) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                    row)
 
     conn.commit()
-
     conn.close()
 
-# Hauptfunktion zum Ausführen des Programms
+
+# Main function to run the program
 def main():
     create_tables()
     insert_data_from_csv()
-    print("Daten der CSV-Dateien erfolgreich in die SQLite3-Datenbank eingefügt!")
+    print("Data from CSV files successfully inserted into the SQLite3 database!")
+
 
 if __name__ == "__main__":
     main()
--- sprit/resources/credentials.py
+++ sprit/resources/credentials.py
@@ -1,10 +1,14 @@
 class Credentials:
-    
-    #API key
+    # API key (normally this key would be stored in a env file and not in the repository)
     tankerkoenig_key = "532bec4c-380c-6f32-d8c7-6a11d0d8b2d5"
-    
+
     # User Agent for Nominatim (geolocation)
     usr_agent = "FH_SWF"
 
     # Path to historical database
-    db_path = "To be defined"
+    db_path = "sprt/data/tk_hist.db"
+
+    # Path to CSV prices file
+    csv_prices_path = "sprit/data/csv"
+
+
